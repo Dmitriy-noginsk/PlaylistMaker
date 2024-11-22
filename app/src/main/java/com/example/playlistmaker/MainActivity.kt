3f2bc6414e27f2ed9fb0b568d277627875886bc4
Dmitriy-noginsk <dmitriy1997noginsk@gmail.com>
2024-11-23 00:44:32 +0300

    Готовое решение1

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,12 +3,7 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-//import android.widget.Button
-//import android.widget.Toast
-//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
@@ -59,37 +54,3 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-//val btnSearch = findViewById<MaterialButton>(R.id.btn_search)
-//val btnLibrary = findViewById<MaterialButton>(R.id.btn_library)
-//val btnSettings = findViewById<MaterialButton>(R.id.btn_settings)
-
-//val buttonClickListener = object : View.OnClickListener {
-//override fun onClick(v: View?) {
-//when (v?.id) {
-//R.id.btn_search -> {
-//Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
-//}
-//R.id.btn_library -> {
-//Toast.makeText(this@MainActivity, "Нажали на Библиотеку!", Toast.LENGTH_SHORT).show()
-//}
-// R.id.btn_settings -> {
-//Toast.makeText(this@MainActivity, "Нажали на Настройки!", Toast.LENGTH_SHORT).show()
-// }
-//}
-//}
-//}
-
-//btnSearch.setOnClickListener(buttonClickListener)
-//btnLibrary.setOnClickListener(buttonClickListener)
-//btnSettings.setOnClickListener(buttonClickListener)
-//}
-
-
-//override fun onCreate(savedInstanceState: Bundle?) {
-//super.onCreate(savedInstanceState)
-//setContentView(R.layout.activity_main)
-
-//findViewById<Button>(R.id.btn_settings).setOnClickListener {
-//startActivity(Intent(this, SettingActivity::class.java))
-//}
-//}
