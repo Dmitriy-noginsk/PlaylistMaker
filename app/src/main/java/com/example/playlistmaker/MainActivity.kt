c5c4d2f35bcee0f0f4199cb975df7068dc4da738
Dmitriy-noginsk <dmitriy1997noginsk@gmail.com>
2025-08-01 14:00:03 -0600

    Домашка Стили и темы

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,12 +3,11 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-//import android.widget.Button
-//import android.widget.Toast
-//import androidx.activity.enableEdgeToEdge
+import android.content.res.Configuration
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-//import androidx.core.view.ViewCompat
-//import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.button.MaterialButton
 
 class SearchActivity : AppCompatActivity() {
@@ -29,8 +28,24 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
         findViewById<View>(R.id.btn_back).setOnClickListener {
             finish()
+        }
+
+        val switchTheme = findViewById<SwitchCompat>(R.id.switch_dark_theme)
+
+        // Инициализируем по текущей теме
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        switchTheme.isChecked = (currentNightMode == Configuration.UI_MODE_NIGHT_YES)
+
+        // Обработка переключения
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
         }
     }
 }
@@ -59,37 +74,3 @@ class MainActivity : AppCompatActivity() {
 }
 
 
-//val btnSearch = findViewById<MaterialButton>(R.id.btn_search)
-//val btnLibrary = findViewById<MaterialButton>(R.id.btn_library)
-//val btnSettings = findViewById<MaterialButton>(R.id.btn_settings)
-
-//val buttonClickListener = object : View.OnClickListener {
-//override fun onClick(v: View?) {
-//when (v?.id) {
-//R.id.btn_search -> {
-//Toast.makeText(this@MainActivity, "Нажали на Поиск!", Toast.LENGTH_SHORT).show()
-//}
-//R.id.btn_library -> {
-//Toast.makeText(this@MainActivity, "Нажали на Библиотеку!", Toast.LENGTH_SHORT).show()
-//}
-// R.id.btn_settings -> {
-//Toast.makeText(this@MainActivity, "Нажали на Настройки!", Toast.LENGTH_SHORT).show()
-// }
-//}
-//}
-//}
-
-//btnSearch.setOnClickListener(buttonClickListener)
-//btnLibrary.setOnClickListener(buttonClickListener)
-//btnSettings.setOnClickListener(buttonClickListener)
-//}
-
-
-//override fun onCreate(savedInstanceState: Bundle?) {
-//super.onCreate(savedInstanceState)
-//setContentView(R.layout.activity_main)
-
-//findViewById<Button>(R.id.btn_settings).setOnClickListener {
-//startActivity(Intent(this, SettingActivity::class.java))
-//}
-//}
